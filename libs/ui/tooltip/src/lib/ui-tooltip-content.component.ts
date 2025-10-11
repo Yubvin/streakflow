6799d133c1b8899efc300d4bf1c9686e5328dcf6
yurii1vynohradov <yurii.vynohradov@gentlecode.com>
2025-10-11 22:04:42 +0300

    refactor(tooltip): extract inline templates to separate .html files
    - Create ui-tooltip.component.html
    - Create ui-tooltip-content.component.html
    
    All Tooltip components now use templateUrl for better maintainability

--- libs/ui/tooltip/src/lib/ui-tooltip-content.component.ts
+++ libs/ui/tooltip/src/lib/ui-tooltip-content.component.ts
@@ -5,16 +5,7 @@ import { CommonModule } from '@angular/common';
   selector: 'ui-tooltip-content',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <div 
-      [attr.data-slot]="'tooltip-content'"
-      [attr.data-side]="side"
-      class="ui-tooltip-content"
-    >
-      {{ content }}
-      <div class="ui-tooltip-arrow"></div>
-    </div>
-  `,
+  templateUrl: './ui-tooltip-content.component.html',
   styleUrls: ['./ui-tooltip.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- libs/ui/tooltip/src/lib/ui-tooltip.component.ts
+++ libs/ui/tooltip/src/lib/ui-tooltip.component.ts
@@ -3,7 +3,7 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 @Component({
   selector: 'ui-tooltip',
   standalone: true,
-  template: '<ng-content></ng-content>',
+  templateUrl: './ui-tooltip.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
     '[attr.data-slot]': '"tooltip"',
